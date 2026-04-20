2779c32dbd15b8a93147f659569c30201d8baf45
Bandit8238 <zech.sq@gmail.com>
2026-04-20 16:00:30 -0600

    StatusCard.jsx

--- components/StatusCard.jsx
+++ components/StatusCard.jsx
@@ -1,1 +1,13 @@
+import { Card, CardContent } from "@/components/ui/card";
 
+export default function StatusCard({ fps, cpu, optimized }) {
+  return (
+    <Card>
+      <CardContent className="p-4">
+        <div>FPS: <b>{fps}</b></div>
+        <div>CPU: <b>{cpu}%</b></div>
+        <div>Status: {optimized ? "Optimizing ⚡" : "Stable"}</div>
+      </CardContent>
+    </Card>
+  );
+}
