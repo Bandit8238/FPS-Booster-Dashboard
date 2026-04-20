d42bd123a2293a4651a8ed6d1df0ecc4ac0b80f3
Bandit8238 <zech.sq@gmail.com>
2026-04-20 15:58:47 -0600

    config | games.js

--- src/config |games.js
+++ src/config |games.js
@@ -1,1 +1,20 @@
-
+export const GAME_TIPS = {
+  Fortnite: [
+    "Lower view distance",
+    "Disable shadows",
+    "Use performance mode",
+    "Cap FPS for stability"
+  ],
+  Valorant: [
+    "Turn off anti-aliasing",
+    "Reduce resolution scale",
+    "Disable V-Sync",
+    "Low material quality"
+  ],
+  Minecraft: [
+    "Reduce render distance",
+    "Turn off smooth lighting",
+    "Use Sodium/OptiFine",
+    "Lower particles"
+  ]
+};
