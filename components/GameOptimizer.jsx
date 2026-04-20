1e9424d0c6c89bb3acadac0f528e966f8cd6dc2a
Bandit8238 <zech.sq@gmail.com>
2026-04-20 16:00:55 -0600

    GameOptimizer.jsx

--- components/GameOptimizer.jsx
+++ components/GameOptimizer.jsx
@@ -1,1 +1,31 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { GAME_TIPS } from "../config/games";
 
+export default function GameOptimizer({ game, setGame }) {
+  return (
+    <Card>
+      <CardContent className="p-4">
+        <h2>Game Optimizer</h2>
+
+        <Select value={game} onValueChange={setGame}>
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+
+          <SelectContent>
+            {Object.keys(GAME_TIPS).map((g) => (
+              <SelectItem key={g} value={g}>{g}</SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        <ul className="mt-3">
+          {GAME_TIPS[game].map((t, i) => (
+            <li key={i}>• {t}</li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+}
