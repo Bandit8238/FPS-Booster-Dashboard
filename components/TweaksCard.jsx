e3459110e24583936064aab8a00cb83059d4cbda
Bandit8238 <zech.sq@gmail.com>
2026-04-20 16:01:24 -0600

    TweaksCard.jsx

--- components/TweaksCard.jsx
+++ components/TweaksCard.jsx
@@ -1,1 +1,16 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { SIMULATED_TWEAKS } from "../config/tweaks";
 
+export default function TweaksCard() {
+  return (
+    <Card>
+      <CardContent className="p-4">
+        <h2>System Tweaks</h2>
+
+        {SIMULATED_TWEAKS.map((t, i) => (
+          <p key={i}>- {t}</p>
+        ))}
+      </CardContent>
+    </Card>
+  );
+}
